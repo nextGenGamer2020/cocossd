a61bc129f18809d8aacd4f5e001e5db361318f3a
nextGenGamer2020 <85717794+nextGenGamer2020@users.noreply.github.com>
2022-04-17 18:23:37 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+status1 = ""
+
+function preload(){
+    img = loadImage("dog_cat.jpg")
+}
+
+function setup(){
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded)
+    canvas = createCanvas(350,350)
+    canvas.center()
+
+}
+
+function draw(){
+    image(img, 0,0, 350,350)
+    fill("#ff00e6")
+    text("Dog", 40,50)
+    noFill()
+    stroke("black")
+    rect(30,40,150,300)
+
+    fill("#0059ff")
+    text("Cat", 200,50)
+    noFill()
+    stroke("black")
+    rect(180,40,150,300)
+
+    fill("#000000")
+    text("Bowl", 185,260)
+    noFill()
+    stroke("black")
+    rect(150,245,70,100)
+}
+
+
+function modelLoaded(){
+    console.log("Model Loaded!")
+    status1 = true
+    objectDetector.detect(img, gotResults)
+}
+
+function gotResults(results){
+    console.log(results)
+}
+
+
